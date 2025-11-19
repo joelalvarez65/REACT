de1871b7b7aeade6e6759d4a29b7f691d42bbce6
Joel Alvarez <alvarezcondorijoeljeremias@gmail.com>
2025-11-19 18:01:12 -0300

    esto tambien

--- src/Circulito.jsx
+++ src/Circulito.jsx
@@ -4,8 +4,8 @@ const CENTRO_Y=window.innerHeight/2
 export function Circulito({ x, y}){
 
   useEffect(()=>{
-  document.addEventListener("mousemove",(e)=>{
-    console.log("se movio el mouse (x:${e.clientX};y:${e.clientY})") 
+  document.addEventListener("mousemove", (e) =>{
+    console.log("se movio el mouse(x:${e.clientX};y:${e.clientY})") 
     console.log("el centro de la pantalla es: ${CENTRO_X} , {CENTRO_X}")
   
     const x = CENTRO_X - e.clientX
@@ -13,7 +13,7 @@ export function Circulito({ x, y}){
   const difuminado = Math.sqrt(x**2 + y**2)
 
   const circulito=document.querySelector("#circulito")
-    circulito.style.boxshadow = "${x}px ${y}px ${difuminado} 10px black"
+    circulito.style.boxShadow = "${x}px ${y}px ${difuminado} 10px black"
 
       })
   },[])
